826b2140f1fe1ecb8d9a27d9c80427d664655ece
Yanni Speron <Yannisperon@gmail.com>
2019-09-13 13:03:52 -0500

    Fixed texture loading

--- OpenGL/src/primitives/ShapeGenerator.cpp
+++ OpenGL/src/primitives/ShapeGenerator.cpp
@@ -42,111 +42,111 @@ ShapeData ShapeGenerator::makeCube()
 	ShapeData ret;
 
 	Vertex positions[] = {
-		glm::vec3(-1.0f, +1.0f, +1.0f), // 0
+		glm::vec3(+0.0f, +0.0f, +0.0f), // 0
 		glm::vec3(+1.0f, +0.0f, +0.0f), // color
-		glm::vec2(-1.0f, +1.0f),        // texCoord
+		glm::vec2(+0.0f, +0.0f),        // texCoord
 
-		glm::vec3(+1.0f, +1.0f, +1.0f), // 1
+		glm::vec3(+1.0f, +0.0f, +0.0f), // 1
 		glm::vec3(+0.0f, +1.0f, +0.0f), // color
-		glm::vec2(-1.0f, +1.0f),        // texCoord
+		glm::vec2(+1.0f, +0.0f),        // texCoord
 
-		glm::vec3(+1.0f, +1.0f, -1.0f), // 2
+		glm::vec3(+1.0f, +1.0f, +0.0f), // 2
 		glm::vec3(+0.0f, +0.0f, +1.0f), // color
-		glm::vec2(-1.0f, +1.0f),        // texCoord
+		glm::vec2(+1.0f, +1.0f),        // texCoord
 
-		glm::vec3(-1.0f, +1.0f, -1.0f), // 3
+		glm::vec3(+0.0f, +1.0f, +0.0f), // 3
 		glm::vec3(+1.0f, +1.0f, +1.0f), // color
-		glm::vec2(-1.0f, +1.0f),        // texCoord
+		glm::vec2(+0.0f, +1.0f),        // texCoord
 
 
 
-		glm::vec3(-1.0f, +1.0f, -1.0f), // 4
+		glm::vec3(+1.0f, +0.0f, +0.0f), // 4
 		glm::vec3(+1.0f, +0.0f, +1.0f), // color
-		glm::vec2(-1.0f, +1.0f),        // texCoord
+		glm::vec2(+0.0f, +0.0f),        // texCoord
 
-		glm::vec3(+1.0f, +1.0f, -1.0f), // 5
+		glm::vec3(+1.0f, +0.0f, +1.0f), // 5
 		glm::vec3(+0.0f, +0.5f, +0.2f), // color
-		glm::vec2(-1.0f, +1.0f),        // texCoord
+		glm::vec2(+1.0f, +0.0f),        // texCoord
 
-		glm::vec3(+1.0f, -1.0f, -1.0f), // 6
+		glm::vec3(+1.0f, +1.0f, +1.0f), // 6
 		glm::vec3(+0.8f, +0.6f, +0.4f), // color
-		glm::vec2(-1.0f, +1.0f),        // texCoord
+		glm::vec2(+1.0f, +1.0f),        // texCoord
 
-		glm::vec3(-1.0f, -1.0f, -1.0f), // 7
+		glm::vec3(+1.0f, +1.0f, +0.0f), // 7
 		glm::vec3(+0.3f, +1.0f, +0.5f), // color
-		glm::vec2(-1.0f, +1.0f),        // texCoord
+		glm::vec2(+0.0f, +1.0f),        // texCoord
 
 
 
-		glm::vec3(+1.0f, +1.0f, -1.0f), // 8
+		glm::vec3(+0.0f, +1.0f, +0.0f), // 8
 		glm::vec3(+0.2f, +0.5f, +0.2f), // color
-		glm::vec2(-1.0f, +1.0f),        // texCoord
+		glm::vec2(+0.0f, +0.0f),        // texCoord
 
-		glm::vec3(+1.0f, +1.0f, +1.0f), // 9
+		glm::vec3(+1.0f, +1.0f, +0.0f), // 9
 		glm::vec3(+0.9f, +0.3f, +0.7f), // color
-		glm::vec2(-1.0f, +1.0f),        // texCoord
+		glm::vec2(+1.0f, +0.0f),        // texCoord
 
-		glm::vec3(+1.0f, -1.0f, +1.0f), // 10
+		glm::vec3(+1.0f, +1.0f, +1.0f), // 10
 		glm::vec3(+0.3f, +0.7f, +0.5f), // color
-		glm::vec2(-1.0f, +1.0f),        // texCoord
+		glm::vec2(+1.0f, +1.0f),        // texCoord
 
-		glm::vec3(+1.0f, -1.0f, -1.0f), // 11
+		glm::vec3(+0.0f, +1.0f, +1.0f), // 11
 		glm::vec3(+0.5f, +0.7f, +0.5f), // color
-		glm::vec2(-1.0f, +1.0f),        // texCoord
+		glm::vec2(+0.0f, +1.0f),        // texCoord
 
 
 
-		glm::vec3(-1.0f, +1.0f, +1.0f), // 12
+		glm::vec3(+1.0f, +0.0f, +1.0f), // 12
 		glm::vec3(+0.7f, +0.8f, +0.2f), // color
-		glm::vec2(-1.0f, +1.0f),        // texCoord
+		glm::vec2(+0.0f, +0.0f),        // texCoord
 
-		glm::vec3(-1.0f, +1.0f, -1.0f), // 13
+		glm::vec3(+0.0f, +0.0f, +1.0f), // 13
 		glm::vec3(+0.5f, +0.7f, +0.3f), // color
-		glm::vec2(-1.0f, +1.0f),        // texCoord
+		glm::vec2(+1.0f, +0.0f),        // texCoord
 
-		glm::vec3(-1.0f, -1.0f, -1.0f), // 14
+		glm::vec3(+0.0f, +1.0f, +1.0f), // 14
 		glm::vec3(+0.4f, +0.7f, +0.7f), // color
-		glm::vec2(-1.0f, +1.0f),        // texCoord
+		glm::vec2(+1.0f, +1.0f),        // texCoord
 
-		glm::vec3(-1.0f, -1.0f, +1.0f), // 15
+		glm::vec3(+1.0f, +1.0f, +1.0f), // 15
 		glm::vec3(+0.2f, +0.5f, +1.0f), // color
-		glm::vec2(-1.0f, +1.0f),        // texCoord
+		glm::vec2(+0.0f, +1.0f),        // texCoord
 
 
 
-		glm::vec3(+1.0f, +1.0f, +1.0f), // 16
+		glm::vec3(+0.0f, +0.0f, +1.0f), // 16
 		glm::vec3(+0.6f, +1.0f, +0.7f), // color
-		glm::vec2(-1.0f, +1.0f),        // texCoord
+		glm::vec2(+0.0f, +0.0f),        // texCoord
 
-		glm::vec3(-1.0f, +1.0f, +1.0f), // 17
+		glm::vec3(+0.0f, +0.0f, +0.0f), // 17
 		glm::vec3(+0.6f, +0.4f, +0.8f), // color
-		glm::vec2(-1.0f, +1.0f),        // texCoord
+		glm::vec2(+1.0f, +0.0f),        // texCoord
 
-		glm::vec3(-1.0f, -1.0f, +1.0f), // 18
+		glm::vec3(+0.0f, +1.0f, +0.0f), // 18
 		glm::vec3(+0.2f, +0.8f, +0.7f), // color
-		glm::vec2(-1.0f, +1.0f),        // texCoord
+		glm::vec2(+1.0f, +1.0f),        // texCoord
 
-		glm::vec3(+1.0f, -1.0f, +1.0f), // 19
+		glm::vec3(+0.0f, +1.0f, +1.0f), // 19
 		glm::vec3(+0.2f, +0.7f, +1.0f), // color
-		glm::vec2(-1.0f, +1.0f),        // texCoord
+		glm::vec2(+0.0f, +1.0f),        // texCoord
 
 
 
-		glm::vec3(+1.0f, -1.0f, -1.0f), // 20
+		glm::vec3(+0.0f, +0.0f, +1.0f), // 20
 		glm::vec3(+0.8f, +0.3f, +0.7f), // color
-		glm::vec2(-1.0f, +1.0f),        // texCoord
+		glm::vec2(+0.0f, +0.0f),        // texCoord
 
-		glm::vec3(-1.0f, -1.0f, -1.0f), // 21
+		glm::vec3(+1.0f, +0.0f, +1.0f), // 21
 		glm::vec3(+0.8f, +0.9f, +0.5f), // color
-		glm::vec2(-1.0f, +1.0f),        // texCoord
+		glm::vec2(+1.0f, +0.0f),        // texCoord
 
-		glm::vec3(-1.0f, -1.0f, +1.0f), // 22
+		glm::vec3(+1.0f, +0.0f, +0.0f), // 22
 		glm::vec3(+0.5f, +0.8f, +0.5f), // color
-		glm::vec2(-1.0f, +1.0f),        // texCoord
+		glm::vec2(+1.0f, +1.0f),        // texCoord
 
-		glm::vec3(+1.0f, -1.0f, +1.0f), // 23
+		glm::vec3(+0.0f, +0.0f, +0.0f), // 23
 		glm::vec3(+0.9f, +1.0f, +0.2f), // color
-		glm::vec2(-1.0f, +1.0f)         // texCoord
+		glm::vec2(+0.0f, +1.0f)         // texCoord
 	};
 
 	ret.numVertices = NUM_ARRAY_ELEMENTS(positions);
@@ -154,19 +154,19 @@ ShapeData ShapeGenerator::makeCube()
 	memcpy(ret.vertices, positions, sizeof(positions));
 
 	GLuint indices[] = {
-		0, 1, 2, // Top face
+		0, 1, 2, // Front face
 		0, 2, 3,
 
-		4, 5, 6, // Front face
+		4, 5, 6, // Right face
 		4, 6, 7,
 
-		8, 9, 10, // Right face
+		8, 9, 10, // Top face
 		8, 10, 11,
 
-		12, 13, 14, // Left face
+		12, 13, 14, // Back face
 		12, 14, 15,
 
-		16, 17, 18, // Back face
+		16, 17, 18, // Left face
 		16, 18, 19,
 
 		20, 22, 21, // Bottom face
